caecc800be467c702c9f29554ea5af05bc115e03
root <ozonneveld@macdon.com>
2025-11-12 14:36:42 +0000

    Re-added ExternalProject_Add so Eigen is discoverable by the build system

--- 3rdparty/eigen/eigen.cmake
+++ 3rdparty/eigen/eigen.cmake
@@ -1,4 +1,15 @@
-include(EternalProject)
+include(ExternalProject)
+
+# 
+ExternalProject_Add(
+    ext_eigen
+    PREFIX eigen
+    SOURCE_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/eigen"
+    UPDATE_COMMAND ""
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+)
 
 # Tells it where Eigen is
 ExternalProject_Get_Property(ext_eigen SOURCE_DIR)
