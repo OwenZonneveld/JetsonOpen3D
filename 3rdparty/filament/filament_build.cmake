a8af623c6b5fff96c00db3008a37c33060d3232e
Owen Zonneveld <owenzonneveld@gmail.com>
2025-12-19 10:27:07 -0600

    Switched to using my custom branches

--- 3rdparty/filament/filament_build.cmake
+++ 3rdparty/filament/filament_build.cmake
@@ -52,8 +52,7 @@ endif()
 ExternalProject_Add(
     ext_filament
     PREFIX filament
-    URL https://github.com/isl-org/filament/archive/d1d873d27f43ba0cee1674a555cc0f18daac3008.tar.gz
-    URL_HASH SHA256=00c3f41af0fcfb2df904e1f77934f2678d943ddac5eb889788a5e22590e497bd
+    URL https://github.com/OwenZonneveld/Jetson_filament/archive/refs/tags/Jetpack_6_2_1.tar.gz
     DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/filament"
     UPDATE_COMMAND ""
     CMAKE_ARGS
