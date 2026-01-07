0123854546d04facdd532eeee2b313aeb39b9f80
Owen Zonneveld <ozonneveld@macdon.com>
2026-01-07 11:08:59 -0600

    Removed headless references in Open3D Filament glue code

--- cpp/open3d/visualization/rendering/filament/FilamentEngine.cpp
+++ cpp/open3d/visualization/rendering/filament/FilamentEngine.cpp
@@ -76,7 +76,7 @@ void EngineInstance::DestroyInstance() { g_instance.reset(); }
 
 /// external function defined in custom Filament EGL backend for headless
 /// rendering
-extern "C" filament::backend::Platform* CreateEGLHeadlessPlatform();
+// extern "C" filament::backend::Platform* CreateEGLHeadlessPlatform();
 
 EngineInstance::EngineInstance() {
     filament::backend::Backend backend = filament::backend::Backend::DEFAULT;
@@ -99,7 +99,7 @@ EngineInstance::EngineInstance() {
     if (is_headless_) {
 #ifdef __linux__
         utility::LogInfo("EGL headless mode enabled.");
-        custom_platform = CreateEGLHeadlessPlatform();
+        // custom_platform = CreateEGLHeadlessPlatform();
 #else
         utility::LogError("EGL Headless is not supported on this platform.");
 #endif
