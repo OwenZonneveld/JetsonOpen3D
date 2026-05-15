6cd28bd39179d3fcd688f6e4dc5352e57e73e978
Owen Zonneveld <ozonneveld@linamar.com>
2026-05-15 10:10:39 -0500

    Updated to custom VTK version

--- 3rdparty/vtk/vtk_build.cmake
+++ 3rdparty/vtk/vtk_build.cmake
@@ -36,8 +36,8 @@ if(BUILD_VTK_FROM_SOURCE)
     ExternalProject_Add(
         ext_vtk
         PREFIX vtk
-        URL https://www.vtk.org/files/release/${VTK_VERSION}/VTK-${VTK_VERSION}.0.tar.gz
-        URL_HASH SHA256=8fed42f4f8f1eb8083107b68eaa9ad71da07110161a3116ad807f43e5ca5ce96
+        # URL https://www.vtk.org/files/release/${VTK_VERSION}/VTK-${VTK_VERSION}.0.tar.gz OLD URL from VTK Source
+        URL https://github.com/OwenZonneveld/Jetson_VTK/archive/refs/tags/v9.1.0_no_logging.tar.gz
         DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/vtk"
         # do not update
         UPDATE_COMMAND ""
